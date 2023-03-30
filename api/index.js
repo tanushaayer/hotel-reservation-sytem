a675064c8983024e7b5cfccaa65b5218836fab86
tanushaayer <tanushastan@gmail.com>
2023-03-30 22:43:31 +0545

    creates middlewares and listens to 8000 port

---
+++ api/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+
+//if type = module rakheko cha bhane package.json ma you can use express function
+// const express = require("express")
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+//if the initial connection is on but some problem in mongoDB this is going to work
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+//middleware
+app.use("/api/auth", authRoute);
+
+app.use(express.json()); //middleware for sending json
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.listen(8000, () => {
+  connect();
+  console.log("connected to backend!!");
+});
